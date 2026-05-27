bcee349c5413cf00c462d6dffbf5afd8e917dc73
Adam Kessel <adam@rosi-kessel.org>
2026-05-27 16:58:54 -0400

    separate out winget build

---
+++ dev/build-winget.ps1
@@ -0,0 +1,41 @@
+#Requires -version 5.1
+Set-Location -Path $PSScriptRoot/..
+Start-Transcript -Path "build-winget.log" -Append
+$initFile = Get-Content ".\gedcom_navigator\__init__.py" -Raw
+if ($initFile -match '__version__\s*=\s*["'']([^"'']+)["'']') {
+   $version = $Matches[1]
+   $fourDigitVersion = "$version.0"
+}
+if ( -not ( $version -and $fourDigitVersion ) ) {
+  write-output "Version not found, exiting."
+  exit 1
+}
+# Fill and commit Winget Manifest
+$installersha256 = Get-FileHash ".\dist\gedcom-navigator-windows-installer.exe" -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+if ( -not ( $installersha256 ) ) {
+   Write-Output "Installer SHA256 not found; skipping Winget manifest generation."
+   exit 1
+}
+Write-Output "Generating Winget manifests..."
+Get-ChildItem -Path .\dev\winget\*.template | ForEach-Object {
+      $manifestTemplate = Get-Content $_.FullName -Raw        
+      $manifest = $manifestTemplate.Replace("{VERSION}", $fourDigitVersion)
+      $manifest = $manifest.Replace("{VERSIONX}", $version)
+      $manifest = $manifest.Replace("{INSTALLERSHA256}", $installersha256)
+      $manifest | Out-File -FilePath ".\dist\$($_.BaseName).yaml" -Encoding utf8
+      Write-Output ("Generated .\dist\$($_.BaseName).yaml")
+  }
+if ( Test-Path -Path "..\winget-pkgs" ) {
+   New-Item -ItemType Directory -Path "..\winget-pkgs\manifests\a\AdamKessel\GEDCOMNavigator" -Force
+}
+if ( Test-Path -Path "..\winget-pkgs\manifests\a\AdamKessel\GEDCOMNavigator" ) {
+    Write-Output "Found local Winget Github manifests directory, copying new manifests... (remember to create pull request for version $fourDigitVersion)"
+    New-Item -ItemType Directory -Path "..\winget-pkgs\manifests\a\AdamKessel\GEDCOMNavigator\$fourDigitVersion" -Force
+    Copy-Item -Path ".\dist\*.yaml" -Destination "..\winget-pkgs\manifests\a\AdamKessel\GEDCOMNavigator\$fourDigitVersion" -Force
+    Set-Location -Path "..\winget-pkgs\manifests\a\AdamKessel\GEDCOMNavigator\$fourDigitVersion"
+    git pull
+    git add *.yaml
+    git commit -m "Update Winget manifests for version $fourDigitVersion"
+    git push
+    Set-Location -Path $PSScriptRoot/..
+}
