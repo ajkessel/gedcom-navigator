ce4fd6aba6b385b86afac03e01eee1e26865003b
Adam Kessel <adam@rosi-kessel.org>
2026-06-01 12:15:13 -0400

    update translations

--- gedcom_navigator/__init__.py
+++ gedcom_navigator/__init__.py
@@ -1,3 +1,3 @@
 """GEDCOM Navigator — powerful tool to explore large GEDCOM family tree files."""
-__version__ = "1.9.11.x"
-__release_date__ = "2026-06-xx"
+__version__ = "1.9.11"
+__release_date__ = "2026-06-01"
